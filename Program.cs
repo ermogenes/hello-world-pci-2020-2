8c55411ad16624a1e91434e0ea127283f914b451
Ermogenes Palacio <ermogenes@gmail.com>
2020-08-28 21:56:00 -0300

    Adiciona versão inicial do programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_world_pci_2020_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World com Git e GitHub!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # hello-world-pci-2020-2
 Programa Hello World da turma 2020-2 de PC1 da Etec Adolpho Berezin
+---
+Criado na aula dos professores Ermogenes e Neri
